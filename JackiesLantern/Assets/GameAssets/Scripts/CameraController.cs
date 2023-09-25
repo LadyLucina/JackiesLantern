492f7a30dc70b2a0b80e74c41291ec12959f1b35
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-09-24 21:15:11 -0500

    Camera Controller Script Added
    This script will follow the Players movement by staying locked on an axis that will prevent rotation when the player model rotates.

---
+++ JackiesLantern/Assets/GameAssets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //References the Player
+    public GameObject player;
+    
+    //Creates new Vector3 that determines the cameras position relative to the player
+    [SerializeField]
+    private Vector3 cameraOffset;
+
+    void Update()
+    {
+        //Sets the camera position to the players position + the offset
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
