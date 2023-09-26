5b2bd32861d6c80c454ba9bf8bc51a8c9bdc72f5
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-09-25 21:10:05 -0500

    ADJUSTMENTS MADE
    Readjusted the main menu UI sizing and anchored to the the Canvas. Also adjusted the Player Movement script to work with a sprint/crouch (sneak) function.

--- JackiesLantern/Assets/GameAssets/Scripts/MainMenu.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/MainMenu.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Author: Mya
+
 public class MainMenu : MonoBehaviour
 {
     public void PlayButton()
--- JackiesLantern/Assets/GameAssets/Scripts/ThirdPersonMovement.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/ThirdPersonMovement.cs
@@ -2,25 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Author: Joshua G.
+/*
+ *Author: Joshua G.
+ *Editor: Stephanie M.
+ * Note: Added isCrouching and isSprinting to the script so that the player is able to do both.
+ * Included compatibility for controller support as well.
+ */
 
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
-   
     //references player camera
     public Transform cam;
 
     //Speed variable that dictates how fast the player moves
-    public float speed = 6f;
+    public float speed;
+
+    //Declare crouchSpeed and sprintSpeed
+    public float crouchSpeed = 3f;
+    public float sprintSpeed = 10f;
     
     //variable that sets turn spped
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public bool isCrouching = false;
+    public bool isSprinting = false;
 
     void Update()
     {
+        //Detect crouch input (keyboard: Left Control, controller: B button on controller)
+        bool isCrouchInput = Input.GetKey(KeyCode.LeftControl) || Input.GetButton("Crouch");
+        if (isCrouchInput && !isCrouching)
+        {
+            isCrouching = true;
+            speed = crouchSpeed;
+        }
+        else if (!isCrouchInput && isCrouching)
+        {
+            isCrouching = false;
+            speed = 100f;
+        }
+
+
+        //Detect sprint input (keyboard: Left Shift, controller: Right Trigger)
+        bool isSprintInput = Input.GetKey(KeyCode.LeftShift) || Input.GetButton("Sprint");
+
+        if (isSprintInput && !isSprinting)
+        {
+            isSprinting = true;
+            speed = sprintSpeed;
+        }
+        else if (!isSprintInput && isSprinting)
+        {
+            isSprinting = false;
+            if (!isCrouching)
+            {
+                speed = 100f;
+            }
+        }
+
         //sets horizontal movement using "A" and "D" and arrow keys
         float horizontal = Input.GetAxisRaw("Horizontal");
 
