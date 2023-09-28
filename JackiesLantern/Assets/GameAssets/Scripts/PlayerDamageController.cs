5d72c1264b421452a2261673ce79e50e6c5cd5f3
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-09-28 11:44:18 -0500

    Updated the PlayerDamageController script
    Fixed a minor bug I noticed after previous push

--- JackiesLantern/Assets/GameAssets/Scripts/PlayerDamageController.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/PlayerDamageController.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Author: Stephanie M
+/* Author: Stephanie M.
+ * Details: This script  is designed to handle player interactions with a "Lurker" object in a Unity game. 
+ * It introduces a damage and stun mechanic, along with the ability to destroy the "Lurker" object upon collision.
+ */
+
 
 public class PlayerDamageController : MonoBehaviour
 {
     [Header("Damage & Stunned Stats")]
     [SerializeField] public int damageAmount = 10;
-    [SerializeField] private float stunDuration = 1.5f; 
+    [SerializeField] private float stunDuration = 1.5f;
 
-    private HealthSystem healthSystem; //Reference to the current HealthSystem script
+    private HealthSystem healthSystem; //Reference to the current HealthSystem Script
     private bool isFrozen = false;
     private Vector3 initialPosition;
-    private GameObject lastLurkerHit; //Store the recently collided "Lurker" object
+    private GameObject lastLurkerHit;
+
+    private Rigidbody playerRigidbody; //Reference to the player's Rigidbody component
 
     private void Start()
     {
         healthSystem = GetComponent<HealthSystem>();
+        playerRigidbody = GetComponent<Rigidbody>(); //Get the player's Rigidbody
     }
 
     private void Update()
@@ -27,18 +34,21 @@ public class PlayerDamageController : MonoBehaviour
             stunDuration -= Time.deltaTime;
             if (stunDuration <= 0f)
             {
-                // The stun duration has passed; unfreeze the player.
+                //The stun duration has passed; unfreeze the player.
                 isFrozen = false;
-                Debug.Log("Player is no longer stunned"); //Console log to check if script is functioning as expected. 
+                Debug.Log("Player is no longer stunned");
 
-                // Restore the player's initial position.
+                //Set the player's Rigidbody to kinematic to prevent unwanted forces.
+                playerRigidbody.isKinematic = true;
+
+                //Restore the player's initial position.
                 transform.position = initialPosition;
 
                 //Destroy the recently collided "Lurker" object.
                 if (lastLurkerHit != null)
                 {
                     Destroy(lastLurkerHit);
-                    Debug.Log("Lurker destroyed");  //Console log to check if script is functioning as expected. 
+                    Debug.Log("Lurker destroyed");
                 }
             }
         }
@@ -49,7 +59,7 @@ public class PlayerDamageController : MonoBehaviour
         if (collision.gameObject.CompareTag("Lurker") && !isFrozen)
         {
             healthSystem.damageHealth(damageAmount);
-            Debug.Log("Player is Stunned"); //Console log to check if script is functioning as expected. 
+            Debug.Log("Player is Stunned");
 
             //Store the player's initial position.
             initialPosition = transform.position;
@@ -66,6 +76,9 @@ public class PlayerDamageController : MonoBehaviour
     {
         isFrozen = true;
         stunDuration = 1.5f;
-        Debug.Log("Player is Stunned"); //Console log to check if script is functioning as expected. 
+        Debug.Log("Player is Stunned");
+
+        //Set the player's Rigidbody to kinematic during the stun to avoid the player ascending once unfrozen
+        playerRigidbody.isKinematic = true;
     }
 }
