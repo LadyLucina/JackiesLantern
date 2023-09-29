fa571664a1c67770958266bf3cb0008dba42e3e0
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-09-29 14:46:46 -0500

    Pause Manager / Scene Loader
    Created and uploaded a pause manager script as well as a scene loader script. This is all testable in the Sandbox scene

---
+++ JackiesLantern/Assets/GameAssets/Scripts/PauseManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Author: Stephanie M.
+ * Details: This script manages the pausing of the game, displaying a pause menu, and handling the
+ * confirmation popup for quitting the game. It provides the user with options to resume the game, 
+ * return to main menu, or quit the game entirely, with appropriate confirmations and pausing/unpausing
+ * of the game as needed.
+ */
+
+public class PauseManager : MonoBehaviour
+{
+    [Header("Pause Menu UI")]
+    //Reference from the Pause Menu Game Object in Inspector
+    public GameObject pauseMenu;
+    //Reference from the Confirmation Pop Up Game Object in Inspector
+    public GameObject confirmQuitPopup;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        //Hide the pause menu and exit confirmation popup
+        pauseMenu.SetActive(false);
+        confirmQuitPopup.SetActive(false);
+    }
+
+    void Update()
+    {
+        //Checks for the ESC key to toggle game pause
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    //Function for paused game
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        pauseMenu.SetActive(true);
+    }
+
+    //Function to resume game from paused state
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        confirmQuitPopup.SetActive(false);
+    }
+
+    //Function to display quit confirmation popup
+    public void ShowQuitConfirm()
+    {
+        confirmQuitPopup.SetActive(true);
+    }
+
+    //This function will be called when the user confirms they wish to exit and return to Main Menu
+    public void ReturnToMenu()
+    {
+        //Unpause the game
+        Time.timeScale = 1;
+
+        //Loads Main Menu scene
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    //This function will be called when the user decides to cancel the quit request
+    public void QuitCancelled()
+    {
+        confirmQuitPopup.SetActive(false);
+    }
+
+    //Function to quit the game
+    public void QuitGame()
+    {
+        //Show the exit confirmation popup for quitting
+        ShowQuitConfirm();
+    }
+
+    //This function will be called when the user confirms they wish to quit the game entirely
+    public void ConfirmQuit()
+    {
+        Debug.Log("Application closing...");
+        //Quit the application
+        Application.Quit();
+    }
+}
+
--- JackiesLantern/Assets/GameAssets/Scripts/SceneLoader.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/SceneLoader.cs
@@ -2,38 +2,20 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Author: Jett G
+//Author: Stephanie M.
 
 public class SceneLoader : MonoBehaviour
 {
-    public string sceneToLoad; // Name of the scene to load
-    public GameObject loadingUI; // Reference to the loading UI (Canvas)
+    [Tooltip("Name of Scene to Load on Collision")]
+    public string sceneToLoad;
 
-    // Call this function to load the scene
-    public void LoadScene()
+    private void OnTriggerEnter(Collider other)
     {
-        // Show the loading UI
-        loadingUI.SetActive(true);
-
-        // Use a Coroutine to load the scene asynchronously
-        StartCoroutine(LoadSceneAsync());
-    }
-
-    private IEnumerator LoadSceneAsync()
-    {
-        // Load the scene asynchronously in the background
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
-
-        // Wait until the asynchronous operation is complete
-        while (!asyncLoad.isDone)
+        //Checks that the colliding object has the tag "Player"
+        if (other.CompareTag("Player"))
         {
-            // Calculate the loading progress and update the UI (e.g., a loading bar)
-            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f); // 0.9 is the maximum progress value
-
-            // Update your loading UI (e.g., set a loading bar fill amount)
-            // loadingBar.fillAmount = progress;
-
-            yield return null; // Wait for the next frame
+            //Load specified scene - Can be adjusted within Inspector
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
