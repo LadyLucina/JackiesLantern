3e0bdee62c036c55ef448cc7e189ece5fced0261
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-09-29 15:10:03 -0500

    Main Menu Script Update
    Added delay so that the On Click () sound can be heard. Delay can be adjusted within Inspector

--- JackiesLantern/Assets/GameAssets/Scripts/MainMenu.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/MainMenu.cs
@@ -3,32 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Author: Mya
+/* Author: Mya
+ * Details: Designed to manage the main menu interactions and scene transitions
+ * with a specified time delay to allow On Click() audio be heard. 
+ * Edited by: Stephanie M
+ */
 
 public class MainMenu : MonoBehaviour
 {
+    //Determine (within Inspector) how long to delay the scene load in seconds
+    [Tooltip("Delay (in seconds) before loading the scene")]
+    public float delayTime = 0.0f;
+
     public void PlayButton()
     {
-        SceneManager.LoadScene("Level 1");
+        //Load Level 1
+        StartCoroutine(LoadSceneWithDelay("Level 1"));
     }
 
     public void SettingsButton()
     {
-        SceneManager.LoadScene("Settings");
+        //Load Settings scene
+        StartCoroutine(LoadSceneWithDelay("Settings"));
     }
 
     public void HelpButton()
     {
-        SceneManager.LoadScene("Help");
+        //Load Help Scene
+        StartCoroutine(LoadSceneWithDelay("Help"));
     }
 
     public void MainMenuButton()
     {
-        SceneManager.LoadScene("MainMenu");
+        //Load Main Menu Scene
+        StartCoroutine(LoadSceneWithDelay("MainMenu"));
     }
 
     public void ExitButton()
     {
         Application.Quit(); 
+    }
+
+    //Coroutine to load a scene with a specified delay
+    private IEnumerator LoadSceneWithDelay(string sceneName)
+    {
+        //Wait for the specified delay time
+        yield return new WaitForSeconds(delayTime);
+
+        //Load the specified scene after the delay
+        SceneManager.LoadScene(sceneName);
     }
 }
