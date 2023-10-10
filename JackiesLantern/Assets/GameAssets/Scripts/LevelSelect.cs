920d7bf9bde954d61e7645a00187dce8b024f1ed
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-10-09 22:24:48 -0500

    Level Loader Script
    Just a small script to show the levels 1 by 1 for weekly builds - To be replaced with an actual level completion script in future

---
+++ JackiesLantern/Assets/GameAssets/Scripts/LevelSelect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        //Load level 1 scene
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        //Load level 2 scene
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        //Load level 3 scene
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void LoadLevel4()
+    {
+        //Load level 4 scene
+        SceneManager.LoadScene("Level 4");
+    }
+}
