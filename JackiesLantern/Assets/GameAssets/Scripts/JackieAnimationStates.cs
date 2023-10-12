94b8a4bd0ab4eca9ed0a20f6f67c0349979fbba9
P.Sipes <p.a.sipes@gmail.com>
2023-10-12 16:53:58 -0500

    Jackie animation stuff because I kibitz

---
+++ JackiesLantern/Assets/GameAssets/Scripts/JackieAnimationStates.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackieAnimationStates : MonoBehaviour
+{
+    private Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if sprinting
+        if (Input.GetButton("Sprint"))
+        {
+            myAnim.SetBool("isRunning", true);
+            myAnim.SetBool("isIdle", false);
+            myAnim.SetBool("isWalking", false);
+        }
+        //if any movement--other special movement, like crouch would go between these two
+        else if (Input.GetAxisRaw("Horizontal") > .01f || Input.GetAxisRaw("Horizontal") < -.01f || Input.GetAxisRaw("Vertical") > .01f || Input.GetAxisRaw("Vertical") < -.01f)
+        {
+            myAnim.SetBool("isRunning", false);
+            myAnim.SetBool("isIdle", false);
+            myAnim.SetBool("isWalking", true);
+        }
+        //if no movement
+        else
+        {
+            myAnim.SetBool("isRunning", false);
+            myAnim.SetBool("isIdle", true);
+            myAnim.SetBool("isWalking", false);
+        }
+        
+    }
+}
