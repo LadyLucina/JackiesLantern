1d2414563ec9286a70ea10c6c15d98c0acae17ba
Mya <88358077+Vixel352@users.noreply.github.com>
2023-11-06 22:36:42 -0600

    Finalized Lurker Portal Effect

--- JackiesLantern/Assets/GameAssets/Scripts/Misc Scripts/LurkerPortal.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/Misc Scripts/LurkerPortal.cs
@@ -2,44 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: Mya
+ * DESCRIPTION: Plays the LurkerPortal particle effect when player is in range of a lurker.
+*/
+
 public class LurkerPortal : MonoBehaviour
 {
-    
-    public Material Material1;
-    public Material Material2;
-    public Renderer Object;
 
     public ParticleSystem Portal;
 
     private void OnTriggerEnter(Collider oher)
     {
-        //Object.material = Material1;  //Makes VFX visible
-        Portal.Play();
+
+        Portal.Play();  //Plays portal particle effects
 
     }
 
 
-
-
-        // Start is called before the first frame update
-        void Start()
+    void Start()
     {
         Portal.Pause();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-
     }
 
 
     public void Dissapear()
     {
-        //Destroy(LurkerPortal);
-        Portal.Pause();
+        Portal.Pause(); //Destroy's particle effect after hand animation finishes playing
     }
 
     
