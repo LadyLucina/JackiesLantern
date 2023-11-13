af1498fb8f4914940b7fd1fac0b230edbc242ce5
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-11-12 20:04:11 -0600

    Survey Link Added

---
+++ JackiesLantern/Assets/GameAssets/Scripts/Misc Scripts/SurveyScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurveyScript : MonoBehaviour
+{
+    void Start()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/1fydUYBS0Isn02jkL9NxXt3e1VIyB0MV4jfAZ6Zys6hs/edit");
+    }
+}
