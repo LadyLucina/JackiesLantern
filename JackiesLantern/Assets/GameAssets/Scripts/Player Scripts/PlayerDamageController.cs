016de78eb0f327b807d17048220e890a17c557f6
Stephanie <128554864+LadyLucina@users.noreply.github.com>
2023-12-10 17:47:59 -0600

    Updated PDC & TPM Scripts

--- JackiesLantern/Assets/GameAssets/Scripts/Player Scripts/PlayerDamageController.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/Player Scripts/PlayerDamageController.cs
@@ -22,20 +22,11 @@ public class PlayerDamageController : MonoBehaviour
     private int skullyDamage = 1;
     private int farmerDamage = 2;
     private int bossDamage = 3;
-    private float initialStunDuration = 0.5f;
     #endregion  
-
-    public bool isStunned = false; //Flag to control the player's stunned state
-    public bool isFrozen = false; //Flag to control the player's frozen state
-    private float stunTimer;
-
-    private Vector3 initialPosition;
-    private GameObject lastEnemyHit;
-    private CharacterController characterController; //Reference to the CharacterController
 
     [Header("Respawn Settings")]
     public float respawnDelay = 3.0f; //Delay before the enemy respawns
-    private bool isRespawning = false; //Flag to control the respawn state
+    public bool isRespawning = false; //Flag to control the respawn state
 
     [Header("Damage Cooldown Setting")]
     [SerializeField] private float damageCooldown = 3.0f; //Damage cooldown timer
@@ -44,8 +35,6 @@ public class PlayerDamageController : MonoBehaviour
     private void Start()
     {
         healthSystem = GetComponent<HealthSystem>();
-        stunTimer = initialStunDuration; //Initialize the stun timer
-        characterController = GetComponent<CharacterController>();
         thirdPersonMovement = GetComponent<ThirdPersonMovement>();
     }
 
@@ -53,42 +42,10 @@ public class PlayerDamageController : MonoBehaviour
     {
         if (isRespawning)
         {
-            //Check if it's time to respawn
             if (Time.time - lastEnemyHitTime >= respawnDelay)
             {
                 isRespawning = false;
-                //Reactivate the enemy object
-                //lastEnemyHit.SetActive(true);
-                //Reset the timer
                 lastEnemyHitTime = 0f;
-            }
-        }
-        else if (isFrozen)
-        {
-            //Decrement the stun timer
-            stunTimer -= Time.deltaTime;
-            if (stunTimer <= 0f)
-            {
-                //The stun duration has passed; unfreeze the player
-                isFrozen = false;
-                isStunned = false;
-
-                //Reset the stun timer
-                stunTimer = initialStunDuration;
-
-                //Restore the players initial position
-                transform.position = initialPosition;
-
-                //Deactivate the collided enemy object
-                if (lastEnemyHit != null)
-                {
-                    lastEnemyHit.SetActive(false);
-                    lastEnemyHitTime = Time.time;
-                    isRespawning = true;
-                }
-
-                //Re-enable the CharacterController
-                characterController.enabled = true;
             }
         }
     }
@@ -96,7 +53,7 @@ public class PlayerDamageController : MonoBehaviour
     private float lastEnemyHitTime;
     private void OnTriggerEnter(Collider other)
     {
-        if (!isFrozen && Time.time - lastDamageTime >= damageCooldown)
+        if (Time.time - lastDamageTime >= damageCooldown)
         {
             if (other.gameObject.CompareTag("Lurker"))
             {
@@ -133,15 +90,6 @@ public class PlayerDamageController : MonoBehaviour
             //Show the damage indicator when damage is taken
             damageIndicator.ShowDamageIndicator();
 
-            //Store the player's initial position
-            initialPosition = transform.position;
-
-            //Store the recently collided enemy object
-            lastEnemyHit = enemy;
-
-            //Call the PlayerStun function to freeze the player
-            PlayerStun();
-
             //Update the last damage time
             lastDamageTime = Time.time;
 
@@ -150,13 +98,4 @@ public class PlayerDamageController : MonoBehaviour
             isRespawning = true;
         }
     }
-
-    private void PlayerStun()
-    {
-        isStunned = true; //Set the stunned flag in PlayerDamageController
-        isFrozen = true; //Freeze the player as well
-
-        //Disable the character controller to avoid player movement during the stun
-        characterController.enabled = false;
-    }
-}
+} 
--- JackiesLantern/Assets/GameAssets/Scripts/Player Scripts/ThirdPersonMovement.cs
+++ JackiesLantern/Assets/GameAssets/Scripts/Player Scripts/ThirdPersonMovement.cs
@@ -14,7 +14,6 @@ using UnityEngine.SceneManagement;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    #region Variables and Declarations
     private PlayerDamageController damageController; //Reference to the PlayerDamageController script
     public CharacterController controller;
     public Transform cam;
@@ -24,10 +23,9 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] private float crouchSpeed = 3f;
     [SerializeField] private float sprintSpeed = 10f;
 
-    //variable that sets turn speed
+
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-
 
     public bool isCrouching = false;
     public bool isSprinting = false;
@@ -37,14 +35,10 @@ public class ThirdPersonMovement : MonoBehaviour
     private bool superSpeedEnabled = false;
     private bool isInvincible = false;
 
-
     //Store checkpoint position
     private Vector3 respawnPoint;
 
-    #endregion
-
-    #region Start Function 
-    public void Start()
+    void Start()
     {
         //Get a reference to the PlayerDamageController script
         damageController = GetComponent<PlayerDamageController>();
@@ -52,139 +46,118 @@ public class ThirdPersonMovement : MonoBehaviour
         //Set initial respawn point
         respawnPoint = transform.position;
     }
-    #endregion
 
-    #region Update Function
     void Update()
     {
-        #region Checking is player is crouching or sprinting
-        if (!damageController.isStunned) //Check if the player is not stunned
+        HandleCheats();
+        HandleMovementInput();
+    }
+
+    void HandleMovementInput()
+    {
+        //Detect crouch input (keyboard: Left Control, controller: B button on controller)
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetButtonDown("Crouch"))
         {
-            // Detect crouch input (keyboard: Left Control, controller: B button on controller)
-            if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetButtonDown("Crouch"))
+            isCrouching = !isCrouching;
+            speed = isCrouching ? crouchSpeed : 50f;
+            if (isCrouching)
             {
-                isCrouching = !isCrouching;
-                speed = isCrouching ? crouchSpeed : 50f;
-                if (isCrouching)
-                {
-                    isSprinting = false;
-                }
-            }
-
-
-                //Detect sprint input (keyboard: Left Shift, controller: Right Trigger)
-                if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetButtonDown("Sprint"))
-            {
-                isSprinting = !isSprinting;
-                speed = isSprinting ? sprintSpeed : (isCrouching ? crouchSpeed : 50f);
-                if (isSprinting)
-                {
-                    isCrouching = false;
-                }
-            }
-
-                #endregion
-
-                #region Cheat Key Toggles
-                /*  -----------------
-                 *   CHEAT KEY BINDS
-                 *  -----------------
-                 */
-
-                //Toggle Invinciblity
-                if (Input.GetKeyDown(KeyCode.I))
-            {
-                isInvincible = !isInvincible;
-            }
-
-            // Toggle super speed
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                superSpeedEnabled = !superSpeedEnabled;
-            }
-
-            // Toggle respawn
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                Respawn();
-            }
-
-            // Level loading
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                LoadLevel("Level 1");
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                LoadLevel("Level 2");
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                LoadLevel("Level 3");
-            }
-
-            /*  ----------------
-             *   END OF TOGGLES
-             *  ----------------
-             */
-            #endregion
-
-            #region Player Movement Functions and Methods 
-
-            //Sets horizontal movement using "A" and "D" and arrow keys
-            float horizontal = Input.GetAxisRaw("Horizontal");
-
-            //Sets vertical movement using "W" "S" and arrow keys
-            float vertical = Input.GetAxisRaw("Vertical");
-
-            //Get input direction
-            Vector3 inputDirection = new Vector3(horizontal, 0f, vertical);
-
-            //Check if the player is walking backward
-            isWalkingBackward = vertical < 0;
-
-            if (inputDirection.magnitude >= 0.1f)
-            {
-                //Calculate the target angle based on the camera direction
-                float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
-                //Conditionally apply rotation only if not walking backward
-                if (!isWalkingBackward)
-                {
-                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                    transform.rotation = Quaternion.Euler(0f, angle, 0f);
-                }
-
-                //Calculate the movement direction based on the rotated angle
-                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-                //Apply gravity
-                float gravity = Physics.gravity.y;
-                moveDir.y = gravity;
-
-                //Move the character in the calculated direction
-                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+                isSprinting = false;
             }
         }
-        #endregion
-    }
-    #endregion
 
-    #region Stun, Respawn, Load Level, Invincible, and Crouching Functions
-    //Reference to PlayerStun function in PlayerDamageController
-    public void PlayerStun()
+        //Detect sprint input (keyboard: Left Shift, controller: Right Trigger)
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetButtonDown("Sprint"))
+        {
+            isSprinting = !isSprinting;
+            speed = isSprinting ? sprintSpeed : (isCrouching ? crouchSpeed : 50f);
+            if (isSprinting)
+            {
+                isCrouching = false;
+            }
+        }
+
+        //Sets horizontal movement using "A" and "D" and arrow keys
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        //Sets vertical movement using "W" "S" and arrow keys
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //Get input direction
+        Vector3 inputDirection = new Vector3(horizontal, 0f, vertical);
+
+        //Check if the player is walking backward
+        isWalkingBackward = vertical < 0;
+
+        if (inputDirection.magnitude >= 0.1f)
+        {
+            //Calculate the target angle based on the camera direction
+            float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            //Conditionally apply rotation only if not walking backward
+            if (!isWalkingBackward)
+            {
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            }
+
+            //Calculate the movement direction based on the rotated angle
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            //Apply gravity
+            float gravity = Physics.gravity.y;
+            moveDir.y = gravity;
+
+            //Move the player in the calculated direction
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+
+#if UNITY_EDITOR
+    void HandleCheats()
     {
-        damageController.isStunned = true;
-    }
+        //Toggle Invincibility
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            isInvincible = !isInvincible;
+        }
 
-    private void Respawn()
+        //Toggle super speed
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            superSpeedEnabled = !superSpeedEnabled;
+        }
+
+        //Toggle respawn
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Respawn();
+        }
+
+        //Level loading
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            LoadLevel("Level 1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            LoadLevel("Level 2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            LoadLevel("Level 3");
+        }
+    }
+#endif
+
+    void Respawn()
     {
         transform.position = respawnPoint;
     }
 
-    private void LoadLevel(string levelName)
+    void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
     }
@@ -198,5 +171,4 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         return isCrouching;
     }
-    #endregion
 }
